d88682a479a360a9858a9a15ad5174dfc5a1a666
dan <dis.131998@gmail.com>
2025-09-18 17:44:38 +0300

    add: extension to filter todos

--- src/extensions.js
+++ src/extensions.js
@@ -1,7 +1,27 @@
+import { isToday, isThisWeek } from "date-fns";
+
 export const Extensions  = (() => {
     const where = function(arr, hash={}) {
         const key = Object.keys(hash)[0]
         return arr.filter(item => item[key] === hash[key])
     };
-    return { where }
+    const highPriority = (arr) => {
+        return where(arr, {"priority": "high"})
+    }
+    const completed = (arr) => {
+        return where(arr, {"completed": true})
+    }
+    const today = (arr) => {
+        return arr.filter(e => {
+            return isToday(new Date(e.year, e.month, e.day)) >= 1;
+        })
+    }
+    const upcoming = (arr) => {
+        return arr.filter(e => {
+            if (!isToday(new Date(e.year, e.month, e.day)) >= 1) {
+                return isThisWeek(new Date(e.year, e.month, e.day))
+            };
+        })
+    }
+    return { where, highPriority, completed, today, upcoming }
 })()
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { TodosController } from "./controllers/todos_controller.js"
 import { MenuComponent } from "./components/menu.js"
 import { TodosComponent } from "./components/todos.js"
 import { PubSub } from "./pubsub/pubsub.js"
-import { isToday, isThisWeek } from "date-fns";
+
 
 class App {
     constructor () {
@@ -21,53 +21,49 @@ class App {
         
        
 
-        this.menu = new MenuComponent(this.projects_controller.index, this.projects_controller.show)
-        this.todos = new TodosComponent(this.projects_controller.show.todos);
-            this.menu.fillProject(this.projects_controller.show)
-            this.menu.render()
-            this.todos.render()
+        this.menuComponent = new MenuComponent(this.projects_controller.index, this.projects_controller.show)
+        this.todosComponent = new TodosComponent(this.projects_controller.show.todos);
+            this.menuComponent.fillProject(this.projects_controller.show)
+            this.menuComponent.render()
+            this.todosComponent.render()
 
-            this.menu.bind()
-            this.todos.bind()  
+            this.menuComponent.bind()
+            this.todosComponent.bind()  
     };
 
     init () {   
+        let tasks = this.todos_controller.index
+        let projects = this.projects_controller.index
 
         PubSub.subscribe("project:add", (name) => this.projects_controller.new(name));
         PubSub.subscribe("project:destroy", () => this.projects_controller.destroy());
         PubSub.subscribe("app:update_project", (id) => this.#updateProject(id));
         PubSub.subscribe("project:last", () => {
-            this.#updateProject(this.projects_controller.index.at(-1).id)})
+            this.#updateProject(projects.at(-1).id)})
        
 
 
         PubSub.subscribe("todos:updated", () => this.#todosUpdated())
         PubSub.subscribe("todos:show_important", () => {
-            this.todos.todos = this.todos_controller.index.where({"priority": "high"})
-            this.todos.render()
-            this.menu.fillProject("", "Important")
+            this.todosComponent.todos = Extensions.highPriority(tasks)
+            this.todosComponent.render()
+            this.menuComponent.fillProject("", "Important")
         })
 
         PubSub.subscribe("todos:show_completed", () => {
-            this.todos.todos = this.todos_controller.index.where({"completed": true})
-            this.todos.render()
-            this.menu.fillProject("", "Completed")
+            this.todosComponent.todos = Extensions.completed(tasks)
+            this.todosComponent.render()
+            this.menuComponent.fillProject("", "Completed")
         })
         PubSub.subscribe("todos:show_today", () => {
-            this.todos.todos = this.todos_controller.index.filter(todo => {
-                return isToday(new Date(todo.year, todo.month, todo.day)) >= 1;
-            })
-            this.todos.render()
-            this.menu.fillProject("", "Today")
+            this.todosComponent.todos = Extensions.today(tasks)
+            this.todosComponent.render()
+            this.menuComponent.fillProject("", "Today")
         })
         PubSub.subscribe("todos:show_upcoming", () => {
-            this.todos.todos = this.todos_controller.index.filter(todo => {
-            if (!isToday(new Date(todo.year, todo.month, todo.day)) >= 1) {
-            return isThisWeek(new Date(todo.year, todo.month, todo.day))
-            };
-            })
-            this.todos.render()
-            this.menu.fillProject("", "Upcoming")
+            this.todosComponent.todos = Extensions.upcoming(tasks)
+            this.todosComponent.render()
+            this.menuComponent.fillProject("", "Upcoming")
         })
         PubSub.subscribe("todo:add", (data) => this.todos_controller.new(data));
         PubSub.subscribe("todo:toogle", (id) => {
@@ -90,16 +86,16 @@ class App {
 
     #updateProject(id) {
         this.projects_controller.setProject(id);
-        this.menu.currentProject = this.projects_controller.show
-        this.menu.fillProject(this.projects_controller.show)
+        this.menuComponent.currentProject = this.projects_controller.show
+        this.menuComponent.fillProject(this.projects_controller.show)
 
         this.todos_controller.projectId = id;
-        this.todos.todos = this.projects_controller.show.todos
-        this.todos.render()
+        this.todosComponent.todos = this.projects_controller.show.todos
+        this.todosComponent.render()
     };
     #todosUpdated() {
-        this.todos.todos = this.projects_controller.show.todos;
-        this.todos.render();
+        this.todosComponent.todos = this.projects_controller.show.todos;
+        this.todosComponent.render();
     };
 };
 
